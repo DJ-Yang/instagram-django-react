f2a74d5ae304056daf18986a55baeb265022f53b
DJ-Yang <dong3831@hanmail.net>
2020-11-05 13:36:07 +0900

    add unfollow

--- instagram/views.py
+++ instagram/views.py
@@ -8,12 +8,21 @@ from django.contrib import messages
 
 from django.contrib.auth import get_user_model
 
+from django.db.models import Q
+
 @login_required
 def index(request):
+  post_list = Post.objects.all()\
+        .filter(
+          Q(author=request.user) |
+          Q(author__in=request.user.following_set.all())
+        )
+
   suggested_user_list = get_user_model().objects.exclude(pk=request.user.pk)\
   .exclude(pk__in=request.user.following_set.all())[:3]
 
   return render(request, "instagram/index.html", {
+    'post_list': post_list,
     "suggested_user_list": suggested_user_list,
   })
 
@@ -47,10 +56,18 @@ def post_detail(request, pk):
 
 def user_page(request, username):
   page_user = get_object_or_404(get_user_model(), username=username, is_active=True)
+
   post_list = Post.objects.filter(author=page_user)
   post_list_count = post_list.count()
+
+  if request.user.is_authenticated:
+    is_follow = request.user.following_set.filter(pk=page_user.pk).exists()
+  else:
+    is_follow = False
+
   return render(request, "instagram/user_page.html", {
     "page_user": page_user,
     "post_list": post_list,
     "post_list_count": post_list_count,
+    "is_follow": is_follow,
   })
